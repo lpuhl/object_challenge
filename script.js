c04aa570e4f0a9337d4ee3650e819f7ac899f51a
Liz Puhl <eapuhl@gmail.com>
2016-07-28 09:27:51 -0500

    object challenge completed

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ var locations = []; // this array will hold your objects
 
 // object constructor
 
+function State(state, revenue, locationCount) {
+	this.state = state;
+	this.revenue = revenue;
+	this.locationCount = locationCount;
+	this.averageRevenue = function(){
+		return revenue / locationCount;
+	}
+}
+
 // object instances
 
+var illinois = new State("IL", 5000, 12);
+
+var minnesota = new State("MN", 300, 3);
+
+var nevada = new State("NV", 25000, 1);
+
 // push object instances to locations array
+
+locations.push(illinois, minnesota, nevada);
+
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
@@ -17,3 +35,21 @@ $(document).ready(function () {
     $('ol').append($li);
   });
 });
+
+
+
+/*
+["IL", 5000, 12]
+["MN", 300, 3]
+["NV", 25000, 1]
+
+Pizza For People wants to use your object knowledge to
+
+• display their locations as a list
+• display the average amount of revenue per location for each chain
+
+Create an object constructor for each state (probably named something like State or Region). The object properties should include state, revenue, locationCount).
+Create a function, within the constructor, that calculates and returns the average revenue. Name this function averageRevenue.
+Create an instance of each object using the provided data.
+
+*/
